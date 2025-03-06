267cad4df9abda07d7d2d5a56b39b7b1fdc6b670
Bruno Pereira <bruno762ix@gmail.com>
2025-03-06 20:52:19 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../projeto de fato/estilo/style.css">
+    <title>Site de fato nego</title>
+</head>
+<body>
+    <header class="header1">
+        <div class="container">
+            <div class="logomarca">
+                <h1>LogoMarca</h1>
+            </div>
+            <div class="menu-desk">
+                <nav class="nav-desktop">
+                    <ul>
+                        <li><a href="index.php">HOME</a></li>
+                        <li><a href="#">SOBRE</a></li>
+                        <li><a href="#">CONTATO</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="clear"></div>
+            <div class="menu-mob">
+                <nav class="nav-mobile">
+                    <ul>
+                        <li><a href="index.php">HOME</a></li>
+                        <li><a href="#">SOBRE</a></li>
+                        <li><a href="#">CONTATO</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="clear"></div>
+
+        </div>
+
+    </header>
+</body>
+</html>
